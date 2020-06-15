17e00fa97c823dff8e001b39cdee8d654416aa47
Bengsiswanto <bengsiswantoh@gmail.com>
2020-06-15 12:10:25 +0700

    use advance template

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 require("dotenv").config();
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+const botCommands = require("./commands");
+
+Object.keys(botCommands).map((key) => {
+  bot.commands.set(botCommands[key].name, botCommands[key]);
+});
+
 const TOKEN = process.env.TOKEN;
 
 bot.login(TOKEN);
@@ -10,15 +17,16 @@ bot.on("ready", () => {
 });
 
 bot.on("message", (msg) => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
-    msg.channel.send("pong");
-  } else if (msg.content.startsWith("!kick")) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply("Please tag a valid user!");
-    }
+  const args = msg.content.split(/ +/);
+  const command = args.shift().toLowerCase();
+  console.info(`Called command: ${command}`);
+
+  if (!bot.commands.has(command)) return;
+
+  try {
+    bot.commands.get(command).execute(msg, args);
+  } catch (error) {
+    console.error(error);
+    msg.reply("there was an error trying to execute that command!");
   }
 });
