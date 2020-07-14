e6cc0745cbe85040d32da0948743c96488e01883
Bengsiswanto <mightiesthero@gmail.com>
2020-07-14 20:28:16 +0700

    remove api

--- api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
-
-const { getPrice } = require('./endpoints/yahoo.js');
-
-app.get('/', async (req, res) => res.send(await getPrice()));
-
-app.listen(port, () => console.log(`App started`));
