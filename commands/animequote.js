d92df5d50cad3c24bc9967f181bb2e2bf97d24d7
Bengsiswanto <mightiesthero@gmail.com>
2021-01-04 21:09:24 +0700

    fix handle response from animequote

--- commands/animequote.js
+++ commands/animequote.js
@@ -7,11 +7,15 @@ module.exports = {
   name: 'animequote',
   description: 'Show random anime quote',
   async execute(msg, args) {
-    const response = await axios({ method: 'get', url, baseURL });
-    const quote = response.data[0];
-    let result = `__**${quote.anime}**__\n`;
-    result = `${result}**${quote.character}**\n`;
-    result = `${result}${quote.quote}`;
-    msg.channel.send(result);
+    try {
+      const response = await axios({ method: 'get', url, baseURL });
+      const quote = response.data.data[0];
+      let result = `__**${quote.anime}**__\n`;
+      result = `${result}**${quote.character}**\n`;
+      result = `${result}${quote.quote}`;
+      msg.channel.send(result);
+    } catch (err) {
+      msg.channel.send(err);
+    }
   },
 };
